c08b985892c3153e8aebfb508716047a1581d446
Rohit Chauhan <rohitchauhan13703@gmail.com>
2025-12-12 19:36:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/super-sonic/', // 🔹 Yaha aapke GitHub repo ka exact name
+  base: '/super-sonic/', // 🔹 Yaha aapke GitHub repo ka exact name dalna hai
 });
